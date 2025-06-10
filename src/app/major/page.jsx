dfa5f8c8fb1dbebe9237691651ba4a4c838bf6a7
KetenZits <165859061+KetenZits@users.noreply.github.com>
2025-06-10 12:27:12 +0700

    fix major title

--- src/app/major/page.jsx
+++ src/app/major/page.jsx
@@ -173,8 +173,8 @@ const page = () => {
                           <div className="flex items-center gap-3">
                             <div className="h-12 w-1 bg-gradient-to-b from-blue-500 to-pink-500 rounded-full"></div>
                             <div>
-                              <h3 className="text-lg md:text-xl font-bold text-slate-800">
-                                {testimonial.author}
+                              <h3 className="text-lg md:text-xl font-bold bg-gradient-to-r from-fuchsia-400 to-blue-400 bg-clip-text text-transparent">
+                                {testimonial.major}
                               </h3>
                               <p className="text-sm md:text-base text-blue-600 font-medium">
                                 {testimonial.class}
